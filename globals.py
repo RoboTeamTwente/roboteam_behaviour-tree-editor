1286922e46bb1607c51e8eedd7015d5ee21f3d70
robzelluf <robzelluf@hotmail.com>
2018-11-29 10:30:35 +0100

    Change spacing in globals

--- globals.py
+++ globals.py
@@ -1,11 +1,14 @@
 import string
 import random
 
+
 def initialize():
     main_window = None
 
+
 def main_window():
     return None
+
 
 def randomID():
     return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(random.randint(13, 17)))
