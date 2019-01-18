4bf18b279f61ab30d39e02a9010c7705a80ae36f
robzelluf <robzelluf@hotmail.com>
2019-01-18 14:00:15 +0100

    Select folder to save for roboteam_ai

--- globals.py
+++ globals.py
@@ -2,6 +2,7 @@ import string
 import random
 import os
 import sys
+from time import sleep
 
 main_window = None
 PROJECT_DIR = os.path.dirname(sys.argv[0])
@@ -11,3 +12,14 @@ if PROJECT_DIR:
 
 def randomID():
     return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(random.randint(13, 17)))
+
+def findJSONDirectory(folder):
+    directory = os.getcwd()
+    while True:
+        directory = os.path.abspath(os.path.join(directory, ".."))
+        if directory.split("/")[-1] == "roboteamtwente":
+            directory += "/workspace/src/roboteam_ai/roboteam_ai/src/jsons/" + folder + "/"
+            if os.path.exists(directory):
+                return directory
+            else:
+                return False
