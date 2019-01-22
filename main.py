d0e13a51dc12a5a4127946ee4792b8f3e8f2e6ab
robzelluf <robzelluf@hotmail.com>
2019-01-22 10:02:52 +0100

    Move config_files from b3e to roboteam_ai

--- main.py
+++ main.py
@@ -11,13 +11,12 @@ import globals
 
 
 def getNodes():
-    DIR = globals.PROJECT_DIR + "config_files/"
-
+    DIR = os.path.abspath(os.path.join(globals.ai_json_folder, "..", "treeinterp/config_files/"))
     types = defaultdict(list)
     nodes = defaultdict(list)
 
     for file in os.listdir(DIR):
-        with open(DIR + file) as f:
+        with open(DIR + "/" + file) as f:
             node_type = file[:-4]
             csv_reader = csv.reader(f)
             for row in csv_reader:
@@ -32,9 +31,9 @@ def main():
     root = Tk()
     root.minsize(width=800, height=600)
     root.winfo_toplevel().title("RTT Behavior Tree Editor")
+    globals.ai_json_folder = globals.findJSONDirectory()
     types, nodes = getNodes()
 
-    globals.ai_json_folder = globals.findJSONDirectory()
     if not globals.ai_json_folder:
         exit("jsons folder in roboteam_ai not found!")
 
